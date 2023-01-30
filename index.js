9808de2fa4046858a125f03f5f448f49f29b13b6
A7madDeveloper <123985171+A7madDeveloper@users.noreply.github.com>
2023-01-30 21:24:33 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const { readdirSync } = require("fs")
+const moment = require("moment");
+const { ActivityType } = require("discord.js")
+
+let token = "your token"
+
+client.on("ready", async () => {
+console.log("activity bot ready >")
+client.user.setActivity({type: ActivityType.Listening, name: "activity bot"})
+})
+
+client.login(token)
